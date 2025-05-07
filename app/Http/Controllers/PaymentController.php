48924853f27919c2b26a8d16501eeebb89842ed8
Raja Asyraf <rmasyraf@gmail.com>
2025-05-07 12:53:42 +0800

    Add logic on webhook handler to authenticate the payload

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -43,11 +43,73 @@ class PaymentController extends Controller
 
     public function xsollaWebhook(Request $request)
     {
+        $headers = $request->headers->all();
+        $body = $request->all();
+
         Log::info('Xsolla webhook received', [
-            'headers' => $request->headers->all(),
-            'payload' => $request->all(),
+            'headers' => $headers,
+            'body' => $body,
         ]);
+
+        if (!isset($headers['authorization'][0])) {
+            return response()->json(['error' => 'Authorization header missing'], 403);
+        }
+
+        // Authenticate webhook by checking the signature
+        $matches = [];
+        preg_match('~^Signature ([0-9a-f]{40})$~', $headers['authorization'][0], $matches);
+        if (array_key_exists(1, $matches)) {
+            $clientSignature = $matches[1];
+        } else {
+            throw new InvalidSignatureException('Signature not found in "Authorization" header from Xsolla webhook request: '.$headers['authorization']);
+        }
+        $expectedSignature = sha1(json_encode($body) . env('XSOLLA_WEBHOOK_SECRET'));
+
+        if (!hash_equals($clientSignature, $expectedSignature)) {
+            return response()->json(['error' => [
+                    'code' => 'INVALID_SIGNATURE',
+                    'message' => 'Invalid signature',
+                ]
+            ], 403);
+        }
+
+        // Differentiate the notification type (user_validation, order_paid)
+        $notificationType = $body['notification_type'] ?? null;
+        switch ($notificationType) {
+            case 'user_validation':
+                // Handle user validation
+                $user = $body['user'] ?? null;
+                if (!$user) {
+                    return response()->json(['error' => [
+                        'code' => 'INVALID_USER',
+                        'message' => 'Invalid user ID'
+                    ]
+                    ], 400);
+                }
+                \Log::debug('user id', [
+                    'user_id' => $user['id'],
+                ]);
+                if ($user['id'] !== 'user_id_1') {
+                    return response()->json(['error' => [
+                            'code' => 'INVALID_USER',
+                            'message' => 'Invalid user ID'
+                        ]
+                    ], 400);
+                }
+                break;
+            case 'order_paid':
+                // Handle order paid
+                break;
+            case 'payment':
+                // Handle payment
+                break;
+            default:
+                return response()->json(['error' => 'Unknown notification type'], 400);
+        }
 
         return response()->json(['status' => 'success']);
     }
 }
+
+
+
